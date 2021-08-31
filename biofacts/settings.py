1b93e518e9376383a2ffd5e0ecf0d4d8f31b2a71
Blake R <blakeromano19@gmail.com>
2021-08-30 20:29:39 -0400

    heroku config

--- biofacts/settings.py
+++ biofacts/settings.py
@@ -28,6 +28,8 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG') == 'True'
 
+DEBUG_PROPAGATE_EXCEPTIONS = True
+
 ALLOWED_HOSTS = []
 
 
@@ -130,3 +132,6 @@ LOGOUT_REDIRECT_URL = 'home'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import django_heroku
+django_heroku.settings(locals())
